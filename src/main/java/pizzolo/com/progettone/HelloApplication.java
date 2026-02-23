6fa9b1824b1920e8ccd9bc9a73f70bc56d1ed1c4
pizzolo.alex <alex.pizzolo@edu.iisleviponti.it>
2026-02-23 11:43:59 +0100

    Prova animazione

---
+++ src/main/java/pizzolo/com/progettone/HelloApplication.java
@@ -0,0 +1,23 @@
+package pizzolo.com.progettone;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 330);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/pizzolo/com/progettone/controller/HelloController.java
@@ -0,0 +1,35 @@
+package pizzolo.com.progettone.controller;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+public class HelloController {
+    public Pane pane;
+    @FXML
+    private Canvas canvas;
+    @FXML
+    public void inizia() {
+        Circle cr = new Circle(150,150,20, Color.GREENYELLOW);
+        pane.getChildren().add(cr);//aggiunge al figlio, quindi il canvas, il cerchio
+        TranslateTransition fd = new TranslateTransition(Duration.seconds(5), cr);//anima il cerchio
+        //partenza
+        fd.setFromX(200);
+        fd.setFromY(200);
+        //arrivo
+        fd.setToX(88);
+        fd.setToY(120);
+        fd.setInterpolator(Interpolator.LINEAR);
+        fd.play();
+    }
+}
