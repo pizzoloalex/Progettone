058a3b308b9c5e9d9f82f16671605292ca20ef87
pzale <alex.pizzolo@edu.iisleviponti.it>
2026-02-25 21:43:27 +0100

    Ricominciato metodo initialize

--- src/main/java/pizzolo/com/progettone/controller/HelloController.java
+++ src/main/java/pizzolo/com/progettone/controller/HelloController.java
@@ -9,9 +9,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import pizzolo.com.progettone.model.ImmaginiStrada;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -20,88 +22,25 @@ import java.util.ArrayList;
  */
 
 public class HelloController {
+    @FXML
+    private VBox vBox;
+    @FXML
+    private Pane schermo;//mostra tutto il gioco
+    @FXML
+    private VBox mappa;//mostra la mappa
+    @FXML
+    private ImageView macchina;//mostra la macchina
 
-    @FXML
-    private ImageView mappa;//contiene la mappa
-    @FXML
-    //TODO eventualmente si puo aggiungere alla ImageView sopra collegandoli con una collisione
-    private Canvas ostacoli;//serve per disegnare gli ostacoli
-    @FXML
-    private ImageView macchina;//contiene la macchina
-    @FXML
-    private Pane schermo;//schermo di tutto il gioco
-
-    public void initialize() {
-        schermo.setFocusTraversable(true);
-        schermo.requestFocus();
-        schermo.setOnKeyPressed(this::muoviMacchina);
-
-        // Immagine strada
+    public void initialize(){
         Image strada = new Image(getClass().getResource("/pizzolo/com/progettone/images/continuo_strada.png").toExternalForm());
-        mappa.setFitHeight(500);
-        mappa.setFitWidth(500);
-        mappa.setPreserveRatio(true);
-        mappa.setImage(strada);
 
-        //continuo strada
-        Image continuoStrada = new Image(
-                getClass().getResource("/pizzolo/com/progettone/images/continuo_strada.png").toExternalForm()
-        );
+        for (int i = 0; i < 3; i++){
+            ImageView view = new ImageView(strada);
+            view.setFitWidth(100);
+            view.setFitHeight(100);
 
-        double tileHeight = 500; // l’altezza che vuoi visualizzare
-
-        for (int i = 0; i < 4; i++) {
-            ImageView tile = new ImageView(continuoStrada);
-
-            tile.setFitWidth(500);
-            tile.setFitHeight(tileHeight);
-
-            tile.setLayoutX(0);
-            tile.setLayoutY(i * tileHeight);
-
-            schermo.getChildren().add(tile);
+            mappa.getChildren().add(view);
         }
-
-
-
-
-        // Immagine macchina
-        Image img = new Image(getClass().getResource("/pizzolo/com/progettone/images/macchina.png").toExternalForm());
-        macchina.setImage(img);
-        macchina.setFitWidth(100);
-        macchina.setFitHeight(100);
-
-        // Posiziona la macchina sopra la strada
-        macchina.setLayoutX(mappa.getLayoutX() + mappa.getFitWidth() / 2 - macchina.getFitWidth() / 2);
-        macchina.setLayoutY(mappa.getLayoutY() + mappa.getFitHeight() / 2 - macchina.getFitHeight() / 2);
     }
 
-
-    /**
-     * muuove l'immagine della macchina di tot step con i tasti
-     *
-     * @param event
-     */
-    public void muoviMacchina(KeyEvent event) {
-        double step = 10;
-        TranslateTransition tr = new TranslateTransition(Duration.millis(10), schermo);
-
-        if (event.getCode() == KeyCode.W) {
-            tr.setByY(step);
-        }
-        if (event.getCode() == KeyCode.S) {
-            tr.setByY(-step);
-        }
-        if (event.getCode() == KeyCode.A) {
-            //TODO calcolare le curve
-            tr.setByX(-step);
-        }
-        if (event.getCode() == KeyCode.D) {
-            //TODO calcolare le curve
-            tr.setByX(step);
-        }
-        tr.play();
-
-
-    }
 }
