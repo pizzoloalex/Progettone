61beb3b748341bd18a181371b2d70bc4dfd24da5
pzale <alex.pizzolo@edu.iisleviponti.it>
2026-02-26 21:41:02 +0100

    Creazione classe, inizializzato metodo loadImages

--- src/main/java/pizzolo/com/progettone/model/ImmaginiStrada.java
+++ src/main/java/pizzolo/com/progettone/model/ImmaginiStrada.java
@@ -1,9 +1,27 @@
 package pizzolo.com.progettone.model;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
 public class ImmaginiStrada {
     private int id;
+    private Image strada;
+    private ImageView view;
 
-    public ImmaginiStrada(int id) {
+    public ImmaginiStrada(int id, String urlImg) {
         this.id = id;
+        loadImages(urlImg, id);
+    }
+
+    /**
+     * ogni id e per distinguere le immagini
+     * @param url del immagine
+     * @param id di ogni immagine diversa
+     */
+    private void loadImages(String url, int id) {
+        this.id = id;
+        this.strada = new Image(getClass().getResource(url).toExternalForm());
+        this.view.setImage(this.strada);
     }
 }
