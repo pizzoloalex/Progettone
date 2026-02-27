b5021eba3d5764a187dae15e8e09631503aa2369
pzale <alex.pizzolo@edu.iisleviponti.it>
2026-02-27 23:06:41 +0100

    Inizializzato movimento e loopinfinito

--- src/main/java/pizzolo/com/progettone/controller/Controller.java
+++ src/main/java/pizzolo/com/progettone/controller/Controller.java
@@ -34,10 +34,12 @@ public class Controller {
     public void initialize() {
         img_1 = new Mappa(0, "/pizzolo/com/progettone/images/continuo_strada.png");
         img_2 = new Mappa(1, "/pizzolo/com/progettone/images/continuo_strada.png");
+
+        //TODO calcolare meglio il posizionamento delle immagini
         //posiziona le immagini una sotto l'altra
         img_1.getView().setLayoutY(0);
         img_2.getView().setLayoutY(-img_1.getView().getFitHeight()); // altezza della tua immagine
-        //posiziona al centro
+
         double centro = (mappa.getPrefHeight() - img_1.getView().getFitWidth()) / 2;
         img_1.getView().setLayoutX(centro);
         img_2.getView().setLayoutX(centro);
@@ -52,13 +54,60 @@ public class Controller {
         });
     }
 
+    /**
+     * movimento della strada per simulare movimento della macchina
+     * W = avanti
+     * S = indietro
+     * A = sinstra
+     * D = destra
+     * @param event input della tastiera
+     */
     public void movimento(KeyEvent event) {
-        double step = 10;
-//        System.out.println("Premuto" + event.getCode());
-        if (event.getCode() == KeyCode.W) {
-            img_1.getView().setLayoutY(img_1.getView().getLayoutY() + step);
-            img_2.getView().setLayoutY(img_2.getView().getLayoutY() + step);
+        double step = 30;//velocita con cui si muove
+        ImageView immaggine1 = img_1.getView();
+        ImageView immaggine2 = img_2.getView();
+        switch (event.getCode()){
+            case W, UP:
+                //simulazione macchina avanti
+                immaggine1.setLayoutY(immaggine1.getLayoutY() + step);
+                immaggine2.setLayoutY(immaggine2.getLayoutY() + step);
+                loopInfinito();
+                break;
+            case S, DOWN:
+                //simulazione macchina indietro (rallentamento)
+                step = 15;
+                immaggine1.setLayoutY(immaggine1.getLayoutY() - step);
+                immaggine2.setLayoutY(immaggine2.getLayoutY() - step);
+                loopInfinito();
+                break;
+            case A, LEFT:
+                immaggine1.setLayoutX(immaggine1.getLayoutX() - step);
+                immaggine2.setLayoutX(immaggine2.getLayoutX() - step);
+                break;
+            case D, RIGHT:
+                immaggine1.setLayoutX(immaggine1.getLayoutX() + step);
+                immaggine2.setLayoutX(immaggine2.getLayoutX() + step);
+                break;
+            default:
+                System.out.println("Input non valido");
         }
     }
+
+    /**
+     * ogni volta che la strada esce dallo schermo ricompare dal alto
+     */
+    private void loopInfinito(){
+        double altezzaMappa = mappa.getHeight();//altezza della mappa
+        double altezzaImmagine1 = img_1.getView().getLayoutY();
+        double altezzaImmagine2 = img_2.getView().getLayoutY();
+        double altezza = img_1.getView().getFitHeight(); //altezza del immagine da reimpostare
+
+        if (altezzaImmagine1 > altezzaMappa){
+            img_1.getView().setLayoutY(-altezza);
+        }if (altezzaImmagine2 > altezzaMappa){
+            img_2.getView().setLayoutY(-altezza);
+        }
+    }
+
 
 }
