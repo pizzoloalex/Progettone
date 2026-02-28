8730cc8d7322594699ab9e4bb000f23e0704b36f
pzale <alex.pizzolo@edu.iisleviponti.it>
2026-02-28 18:35:45 +0100

    Instanziato metodo aaccelerazione

--- src/main/java/pizzolo/com/progettone/model/Movimento.java
+++ src/main/java/pizzolo/com/progettone/model/Movimento.java
@@ -1,5 +1,6 @@
 package pizzolo.com.progettone.model;
 
+import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -14,34 +15,45 @@ public class Movimento extends AnimationTimer {
 
     private double speed = 10;
     private final Pane mappa;
+    private Mappa[] immagini;
     private Timeline accelerazione; //accelerazione per la macchina
 
-    public Movimento(Pane mappa) {
+    public Movimento(Pane mappa, Mappa[] immagini) {
         this.mappa = mappa;
+        this.immagini = immagini;
     }
 
     /**
      * gestisce i movimenti della macchina ad ogni frame
+     *
      * @param l
      */
     @Override
     public void handle(long l) {
-        mappa.setLayoutY(mappa.getLayoutY() + speed);
-        //TODO inizializzare la logica delle collisioni
+        // Movimento verticale di ogni immagine
+        for (Mappa img : immagini) {
+            img.getView().setLayoutY(img.getView().getLayoutY() + speed);
+            // Loop infinito: quando un'immagine esce, torna sopra
+            if (img.getView().getLayoutY() >= mappa.getHeight()) {
+                img.getView().setLayoutY(img.getView().getLayoutY() - img.getView().getFitHeight() * immagini.length);
+            }
+        }
     }
 
     /**
      * ogni 15 secondi la velocita aumenta
      */
-    public void accelera(){
-        accelerazione = new Timeline(new KeyFrame(Duration.seconds(15), actionEvent -> this.speed += 10));
+    public void accelera() {
+        accelerazione = new Timeline(new KeyFrame(Duration.seconds(3), actionEvent -> this.speed += 10));
+        accelerazione.setCycleCount(Animation.INDEFINITE);
+        accelerazione.play();
     }
 
     /**
      * sposta la mappa a destra
      * simula la macchina che si sposta a destra
      */
-    public void turnRight(){
+    public void turnRight() {
         mappa.setLayoutX(mappa.getLayoutX() + speed);
     }
 
@@ -49,7 +61,7 @@ public class Movimento extends AnimationTimer {
      * sposta la mappa a sinistra
      * simula la macchina che si sposta a sinistra
      */
-    public void turnLeft(){
+    public void turnLeft() {
         mappa.setLayoutX(mappa.getLayoutX() - speed);
     }
 
